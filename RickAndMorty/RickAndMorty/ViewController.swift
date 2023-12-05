7210ebc624d8fe2d582368ada891e456c4dd93f2
berking27 <bekoca@innova.com.tr>
2023-12-05 15:37:13 +0300

    Project Setup and ProgrammaticUI Setup Done

---
+++ RickAndMorty/RickAndMorty/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by Berkin KOCA on 5.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .green
+    }
+}
+
+#Preview {
+    ViewController()
+}
+
